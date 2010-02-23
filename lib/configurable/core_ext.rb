b4bfcc5d95f21016cee0184b9633da7944affc6f
Josh Nesbitt <josh@josh-nesbitt.net>
2010-02-23 17:57:01 +0000

    Tweaks,

--- lib/configurable/core_ext.rb
+++ lib/configurable/core_ext.rb
@@ -5,13 +5,13 @@ module Configurable
     def symbolize_keys!
       self.each { |k, v| self[k.to_sym] = v }
     end
-
+    
     def recursive_symbolize_keys!
       self.symbolize_keys!
       self.values.select{ |value| value.is_a?(Hash) }.each{ |hash| hash.recursive_symbolize_keys! }
     end
     
-    def method_missing(method, *args)
+    def method_missing(method, *args, &block)
       self[method]
     end
   end
